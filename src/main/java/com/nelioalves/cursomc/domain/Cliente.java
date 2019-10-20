a03ad8009a61498c8bc60677bf5211bc2100ea17
Pedro <pedro.famendes@gmail.com>
2019-10-20 00:30:57 -0300

    clientes endpoint

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
 
@@ -30,6 +31,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -37,6 +39,10 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	public Cliente() {
+		
+	}
+	
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, Integer tipo) {
 		super();
 		this.id = id;
--- src/main/java/com/nelioalves/cursomc/domain/Endereco.java
+++ src/main/java/com/nelioalves/cursomc/domain/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable{
 
@@ -23,6 +25,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name ="cliente_id")
 	private Cliente cliente;
@@ -31,6 +34,10 @@ public class Endereco implements Serializable{
 	@JoinColumn(name ="cidade_id")
 	private Cidade cidade;
 
+	public Endereco() {
+		
+	}
+	
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
 		super();
